75f0634860c0bbf47ba04e61b103e2b75297b0e5
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2019-09-12 14:00:22 -0400

    orm

--- config/orm.js
+++ config/orm.js
@@ -1,5 +1,27 @@
 var connection = require("../config/connection");
 
+function createQmarks(num) {
+  var arr = [];
+  for (var i = 0; i < num; i++) {
+    arr.push("?");
+  }
+  return arr.toString();
+}
+
+function translateSql(ob) {
+  var arr = [];
+  for (var key in ob) {
+    var value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
+}
+
 var orm = {
   selectAll: function(table, cb) {
     var dbQuery = "SELECT * FROM " + table + ";";
@@ -30,7 +52,6 @@ var orm = {
       cb(res);
     });
   },
-
   updateOne: function(table, objColVals, condition, cb) {
     var dbQuery =
       "UPDATE " +
@@ -41,7 +62,19 @@ var orm = {
       condition;
 
     console.log(dbQuery);
-    connection.query(dbQuery, vals, function(err, res) {
+
+    connection.query(dbQuery, function(err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
+  deleteOne: function(table, condition, cb) {
+    var dbQuery = "DELETE FROM " + table + " WHERE " + condition;
+    console.log(dbQuery);
+
+    connection.query(dbQuery, function(err, res) {
       if (err) {
         throw err;
       }
@@ -49,3 +82,4 @@ var orm = {
     });
   }
 };
+module.exports = orm;

e2f0196019fb7954c9f02297114e65258f5a3bb9
Shivam Sukhwal <49242766+ShivamSukhwal@users.noreply.github.com>
2019-09-16 19:37:17 -0400

    added gitignore

--- db/schema.sql
+++ db/schema.sql
@@ -7,7 +7,7 @@ CREATE TABLE burgers
     id INT NOT NULL
     AUTO_INCREMENT,
     burger_name VARCHAR
-    (40) NOT NULL,
+    (50) NOT NULL,
     devoured BOOLEAN DEFAULT false,
     PRIMARY KEY
     (id)
